b22a358c6d7b710ae321e6a4355c06c13ad45bb3
Jurgo <jurgo88@gmail.com>
2024-10-17 01:51:20 +0200

    Updated portfolio

---
+++ src/js/portfolio.js
@@ -0,0 +1,62 @@
+const portfolioData = {
+    pravnik: {
+      url: './pravnik.html',
+      title: 'Advokátska kancelária',
+      link: 'https://www.aktrokan.sk'
+    },
+    epassport: {
+      url: './epassport.html',
+      title: 'ePassport'
+    },
+    martinavalach: {
+      url: '/martinavalach.html',
+      title: 'Martin a Valach'
+    },
+    roxy: {
+      url: 'public/roxy.html',
+      title: 'Roxy Catering',
+      link: 'https://www.roxycatering.sk'
+    },
+    shopmaster: {
+      url: 'public/shopmaster.html',
+      title: 'Shopmaster 3000'
+    },
+    sigmatech: {
+      url: '/sigmatech.html',
+      title: 'Sigma Tech',
+      link: 'https://www.sigmatech.site'
+    }
+  };
+  
+  function loadModalContent(content) {
+    console.log('Loading content:', content);
+    const modalBody = document.getElementById('exampleModal').querySelector('.modal-body');
+    const modalTitle = document.getElementById('exampleModal').querySelector('.modal-title');
+    const data = portfolioData[content];
+  
+    if (!data) {
+      modalTitle.textContent = 'Unknown';
+      modalBody.innerHTML = 'Content not available.';
+      return;
+    }
+  
+    fetch(data.url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(dataContent => {
+        modalBody.innerHTML = dataContent;
+        modalTitle.textContent = data.title;
+        if (data.link) {
+          modalTitle.innerHTML += ` <a href="${data.link}" target="_blank" class=""> ${new URL(data.link).hostname}</a>`;
+        }
+      })
+      .catch(error => {
+        console.error('Fetch error:', error);
+        modalBody.innerHTML = 'Ups, niečo sa pokazilo a možnože aj dojebalo. Skúste to nabudúce.';
+      });
+  }
+  
